61893f6e660938e16ffdf57826b2ae4a4e161e69
SharmaRatish <sratish746@gmail.com>
2024-02-06 23:37:28 +0530

    Add project files.

---
+++ ProblemSolving/ArrayProlems/MissingElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.ArrayProlems
+{
+    internal class MissingElement
+    {
+        class MyClass
+        {
+            int [] arr = {1,2,5,8,10 };
+
+            static void main(string[] args)
+            {
+                Console.WriteLine("Array Length" + args[0]);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class Program
+    {
+        int[] arr = { 1, 2, 5, 8, 10 };
+        static void Main(string[] args)
+        {
+           
+
+            
+            
+                Console.WriteLine("Array Length Program");
+                Console.ReadLine();
+            
+
+        }
+    }
+}
